d78b6c587deb19bdc5b97f8bfe8edd5f90d66c18
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-11-19 17:17:12 +0000

    images

--- src/components/store/packages/actions.js
+++ src/components/store/packages/actions.js
@@ -57,7 +57,7 @@ function dummyData() {
         From: "Bremen",
         MinPrice: 70.0,
         Photo:
-          "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAAOgblpc6enb-IWXFXY0e5XIy360-Do0SN_sTtODvy8aoBCTBRnT6OwU35OY0GMeDVfM4a4RKLpVArFvCD3vkAA9f04lvx_jn_GxN5ETGNb08cP_DbN_n_-GY6KZ9yFa9UEhBw8rDIugP6iw24-j3PiG1zGhTGyawqe_SKVRCzXJ2Xkoay5GH6dQ&key=AIzaSyDb1uML_5xvDqtQCWppi0z8MSy5YpN7zvU",
+          "https://www.clubvillamar.com/blog/wp-content/uploads/2018/04/Palma_de_Mallorca.jpg",
         To: "Palma",
         date: "9-11 Nov"
       },
@@ -65,7 +65,7 @@ function dummyData() {
         From: "Bremen",
         MinPrice: 214.0,
         Photo:
-          "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAAwlWbp9DJpsytPPCerncwyX2VmK3aem2o1qVOmbhU66gF61qciZA0blGtC_9eTJePSPqOcs12XjCJ27DtSv3qf6M3rLvB-5CtFyQp9VhqL16-5I3IwiGF2j1Eg_YrnjeUEhCBYU9vP5iuwCYOiw8vuESOGhSmjOM74baGlXntw9y9wASV6sc0Gw&key=AIzaSyDb1uML_5xvDqtQCWppi0z8MSy5YpN7zvU",
+          "https://www.telegraph.co.uk/content/dam/video_previews/x/5/x5cgi0ode66q6vuxezqmehmexwer6bt-xlarge.jpg",
         To: "London",
         date: "9-11 Nov"
       },
@@ -73,7 +73,7 @@ function dummyData() {
         From: "Bremen",
         MinPrice: 215.0,
         Photo:
-          "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAAPQahS6vGD82SpdS40yEciiokTGlOFPmHRD_-VdwYOoy-3JaXj0J72Y9b-G6QSiKE8STER6wA-El3yWCqFbcYy0MSBsbT59w-kaGr6JNGYlBKiYow9WPLgCFWh4y_oqQOEhAE-yg30yBOSw48YqTd2GKFGhQB8Gc3h3hJR1DXx84eG2aCr4iAnQ&key=AIzaSyDb1uML_5xvDqtQCWppi0z8MSy5YpN7zvU",
+          "https://www.iamsterdam.com/media/canals-and-cityscapes/v-c-planyourtrip-merijn-roubroeks.jpg?as=false&h=338&w=600&iar=true",
         To: "Amsterdam",
         date: "9-11 Nov"
       },
@@ -81,7 +81,7 @@ function dummyData() {
         From: "Bremen",
         MinPrice: 280.0,
         Photo:
-          "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAA2UBcAqZmTcgmoeTxyjMY5krXPg3Q4IKtwOWT-nMhkf355usO-FO_UjjJbnaBYGOAjMY5NY50dQYjmpDoFCDLtRZcncelMs9sJilgYTbGYV1g8XlqbIu9Ljx02w1uBKOjEhDOF23QOfTQbt3EBBXL7_ImGhQ2BTBWWna50eCFAbpmODs4GlS8BA&key=AIzaSyDb1uML_5xvDqtQCWppi0z8MSy5YpN7zvU",
+          "https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/Switzerland/zurich-switzerland-aerial.jpg?imwidth=450",
         To: "Zurich",
         date: "9-11 Nov"
       },
@@ -89,7 +89,7 @@ function dummyData() {
         From: "Bremen",
         MinPrice: 314.0,
         Photo:
-          "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAA7-JkyIPq43Zl2pk8ZJVtsX40GnO7pKBcK4svvSaWDl7fhVQ71f0V98CpV_kAegvmtoHjFaZH9OxKL1UbS51c_nsXDer0DH_DAcXpBdWqus5KD9_uzvB10SqoNGboWeNaEhCnxiCqtAXYQcFMSpOwevlKGhTVxKAUev_eFpzesPPtsTX2F1ayfg&key=AIzaSyDb1uML_5xvDqtQCWppi0z8MSy5YpN7zvU",
+          "http://www.nightlife-cityguide.com/wp-content/uploads/2018/08/Cosa-vedere-a-Stoccarda-cosa-visitare-672x372.jpg",
         To: "Stuttgart",
         date: "9-11 Nov"
       },
@@ -97,7 +97,7 @@ function dummyData() {
         From: "Bremen",
         MinPrice: 322.0,
         Photo:
-          "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAAhh0kKhKK1wMrXEpnM_C4SfmfUe5hCqnNCP-ZcskJ3l3_2qhOCQa_tzCNS6TQc1yAbdqlZsLIl4DeDkAhk6F5l_U7uBaCBKXGVVVgcfuN4gc_8AQaq9gxtHJkYYWpcwoIEhAKbC7E2TS8JIXENpEzFLNSGhTuQTAxjCb67hFNBRhByvVWdAZ01w&key=AIzaSyDb1uML_5xvDqtQCWppi0z8MSy5YpN7zvU",
+          "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/22/09/paris.jpg?w968h681",
         To: "Paris",
         date: "9-11 Nov"
       },
@@ -105,7 +105,7 @@ function dummyData() {
         From: "Bremen",
         MinPrice: 551.0,
         Photo:
-          "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CmRaAAAACQzmWTkdP4DgDMYoralWmMA_8WCelCVWD8RELDruT2GdtJ6cIlWmYY_QqmP084zYh0gst3MoD4ziTPilFiv8GDV3bWrL1BfGa_-Opt5erWZELq_8nyTkIXJvf8_LmY2bEhBZO0vH-pb5kippO8hk35-IGhQgYYEENeH-YSUf42i1AGYMm-x8-w&key=AIzaSyDb1uML_5xvDqtQCWppi0z8MSy5YpN7zvU",
+          "https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/Turkey/Istanbul%20lead-xlarge.jpg",
         To: "Istanbul",
         date: "9-11 Nov"
       }
